10ac377d40f8c09e7db74d44ca477494304c93b9
Ekaterina Hunter <ekaterina.hunter@kcl.ac.uk>
2023-12-07 11:52:21 +0000

    Add max_length to description

--- things/forms.py
+++ things/forms.py
@@ -4,5 +4,5 @@ from django import forms
 # Create your forms here.
 class ThingForm(forms.Form):
     name = forms.CharField(label='Name', max_length=35)
-    description = forms.CharField(label='Description', widget=forms.Textarea)
+    description = forms.CharField(label='Description', widget=forms.Textarea, max_length=120)
     quantity = forms.IntegerField(label='Quantity', min_value=0,max_value=50)
