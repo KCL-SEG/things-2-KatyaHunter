ee72edcf04483c88d4f488ff5afb850b1ae66235
Ekaterina Hunter <ekaterina.hunter@kcl.ac.uk>
2023-12-07 11:47:59 +0000

    Create Thing form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,8 @@
 """Forms of the project."""
+from django import forms
 
 # Create your forms here.
+class ThingForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=35)
+    description = forms.CharField(label='Description', widget=forms.Textarea)
+    quantity = forms.IntegerField(label='Quantity', min_value=0,max_value=50)
